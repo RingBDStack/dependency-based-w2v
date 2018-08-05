fa57226a3e1186cc4516086e8279f1ec190222e1
LIN <linzw@sugon-gpu-3.ivic.org.cn>
2018-08-05 23:52:56 +0800

    add vocab.py

--- preprocess/README.md
+++ preprocess/README.md
@@ -17,7 +17,8 @@ All we need is to run `mdeps.java`. **Pay attention to set args[0] of the script
 ### Step 4
 The reason why we count frequency of grammatical relations is for negative sampling during training.</br>
 Run `python count.py --file [file] --quantity [quantity]` to count the frequency of every relation and get the `weightcn.txt`.</br>
+Run `python vocab.py --file [file]` to count the frequency of every vocabulary and get the `vocab.txt`.</br>
 **[file] means the preprocessed file in Step 3(such as `mdeps-demo.txt`), [quantity] means number of lines of `depsl.txt`(default is 5785)**
 
 ### Conclusion
-Here, we get the trainfile in Step3, the `weightcn.txt` in step4. That's all we need for training.
+Here, we get the trainfile in Step3, the `weightcn.txt` and `vocab.txt` in step4. That's all we need for training.
---
+++ preprocess/step4/vocab.py
@@ -0,0 +1,36 @@
+import codecs
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='manual to this script')
+parser.add_argument('--file', type=str, default = None)
+#parser.add_argument('--quantity', type=int, default=5785)
+args = parser.parse_args()
+output = codecs.open('vocab.txt', 'w', 'utf-8')
+dic = {}
+i = 0
+with codecs.open(args.file, 'r', 'utf-8')as f:
+    for lines in f:
+       line = lines.replace('\n', '')
+       if len(line)==0:
+           continue
+       str1 = line.split(' ')
+       i = 0
+       for s in str1:
+           if (i==0) or ((i-1)%2==0):
+               if len(s)==0:
+                   continue
+               if s not in dic:
+                   dic[s] = 1
+               else:
+                   dic[s] = dic[s] + 1
+           i = i + 1
+
+#items=dic.items()
+#backitems=[[v[0],v[1]] for v in items]
+#backitems.sort(reverse=False)
+backitems= sorted(dic.items(), key=lambda d:d[1], reverse = True)
+for i in backitems:
+    output.write(str(i[0])+' '+str(i[1]))
+    output.write('\n')
+output.close()
